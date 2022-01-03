7d27bc3cae481f5ab0e68a4544b3c24634f89c4a
DESKTOP-UU5UEAJ\ecata <ecatalincandea@gmail.com>
2022-01-03 22:02:04 +0200

    Add Login and Register pages

--- queue-management-app/src/Pages/Login.tsx
+++ queue-management-app/src/Pages/Login.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, SyntheticEvent, useState} from "react";
 import {Link} from "react-router-dom";
 
 
@@ -8,12 +8,94 @@ interface LoginProps
 }
 
 export const Login: FC<LoginProps > = ({}) => {
-    return (<div>
-        <h1>Login</h1>
-        <nav>
-            <Link to="/">MainePage</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-        </nav>
-    </div>);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const onSubmit = async (e: SyntheticEvent) => {
+
+        e.preventDefault();
+        setIsSubmitted(true);
+        //Do the action for submitting
+        setIsSubmitted(false);
+    };
+
+    return (
+        <>
+        {/*<Toast/>*/}
+
+
+        <div className={"vertical-center"}>
+            <main className="form-signIn">
+
+                <div className="box mb-5">
+                    <form onSubmit={onSubmit}>
+                        <h1 className=" page-title h3 mb-3 fw-normal">
+                            Please sign in
+                        </h1>
+
+                        <div className="form-floating mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="floatingInput"
+                                placeholder="name@example.com"
+                                onChange={(e) => {
+                                    setEmail(e.target.value);
+                                }}
+                            />
+                            <label htmlFor="floatingInput">Email address</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="floatingPassword"
+                                placeholder="Password"
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}
+                            />
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                        <div className="checkbox mt-3">
+                            <div className="row">
+                                <div
+                                    style={{color: "blue"}}
+                                    onClick={() => {
+                                        console.log("Hello");
+                                    }}
+                                >
+                                    Forget password?
+                                </div>
+                            </div>
+                        </div>
+
+                        <button
+                            className="button-login w-100 btn btn-lg mt-3"
+                            type="submit"
+                            disabled={isSubmitted}
+                        >
+                            Sign in
+                        </button>
+
+                        <div className="checkbox mt-3">
+                            <div className="row">
+                                <Link
+                                    className="col-4"
+                                    style={{color: "blue"}}
+                                    onClick={() => {
+                                        console.log("Hello");
+                                    }}
+                                to={"/register"}
+                                >
+                                    Don't have an account?
+                                </Link>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </main>
+        </div>
+        </>
+    );
 }
