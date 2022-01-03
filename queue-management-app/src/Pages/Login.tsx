0c7a53b364ee0d5cb989a0e8ae0aaa9e10039a50
DESKTOP-UU5UEAJ\ecata <ecatalincandea@gmail.com>
2022-01-03 23:58:58 +0200

    Add bootstrap for the project and modify the design for login and register

--- queue-management-app/src/Pages/Login.tsx
+++ queue-management-app/src/Pages/Login.tsx
@@ -1,13 +1,13 @@
 import {FC, SyntheticEvent, useState} from "react";
 import {Link} from "react-router-dom";
+import "./PagesStyle.css";
 
 
-interface LoginProps
-{
+interface LoginProps {
 
 }
 
-export const Login: FC<LoginProps > = ({}) => {
+export const Login: FC<LoginProps> = ({}) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [isSubmitted, setIsSubmitted] = useState(false);
@@ -21,81 +21,79 @@ export const Login: FC<LoginProps > = ({}) => {
 
     return (
         <>
-        {/*<Toast/>*/}
+            {/*<Toast/>*/}
 
 
-        <div className={"vertical-center"}>
-            <main className="form-signIn">
+            <div className={"vertical-center"}>
+                <main className="form-box">
 
-                <div className="box mb-5">
-                    <form onSubmit={onSubmit}>
-                        <h1 className=" page-title h3 mb-3 fw-normal">
-                            Please sign in
-                        </h1>
+                    <div className="box mb-5">
+                        <form onSubmit={onSubmit}>
+                            <h1 className=" page-title h3 mb-3 fw-normal">
+                                Welcome!
+                            </h1>
 
-                        <div className="form-floating mb-3">
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="floatingInput"
-                                placeholder="name@example.com"
-                                onChange={(e) => {
-                                    setEmail(e.target.value);
-                                }}
-                            />
-                            <label htmlFor="floatingInput">Email address</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input
-                                type="password"
-                                className="form-control"
-                                id="floatingPassword"
-                                placeholder="Password"
-                                onChange={(e) => {
-                                    setPassword(e.target.value);
-                                }}
-                            />
-                            <label htmlFor="floatingPassword">Password</label>
-                        </div>
-                        <div className="checkbox mt-3">
-                            <div className="row">
-                                <div
-                                    style={{color: "blue"}}
-                                    onClick={() => {
-                                        console.log("Hello");
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="floatingInput"
+                                    placeholder="name@example.com"
+                                    onChange={(e) => {
+                                        setEmail(e.target.value);
                                     }}
-                                >
-                                    Forget password?
+                                />
+                                <label htmlFor="floatingInput">Email address</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="floatingPassword"
+                                    placeholder="Password"
+                                    onChange={(e) => {
+                                        setPassword(e.target.value);
+                                    }}
+                                />
+                                <label htmlFor="floatingPassword">Password</label>
+                            </div>
+                            <div className="checkbox mt-3">
+                                <div className="row">
+                                    <div className={"col-12"}>
+                                        <Link
+                                            className="text-link"
+                                            to={"/forgetPassword"}
+                                        >
+                                            Forget password?
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <button
-                            className="button-login w-100 btn btn-lg mt-3"
-                            type="submit"
-                            disabled={isSubmitted}
-                        >
-                            Sign in
-                        </button>
+                            <button
+                                className="submit-button w-100 btn btn-lg mt-3"
+                                type="submit"
+                                disabled={isSubmitted}
+                            >
+                                Sign in
+                            </button>
 
-                        <div className="checkbox mt-3">
-                            <div className="row">
-                                <Link
-                                    className="col-4"
-                                    style={{color: "blue"}}
-                                    onClick={() => {
-                                        console.log("Hello");
-                                    }}
-                                to={"/register"}
-                                >
-                                    Don't have an account?
-                                </Link>
+                            <div className="checkbox mt-3">
+                                <div className="row">
+                                    <div className={"col-12"}>
+                                        <Link
+                                            className="text-link"
+                                            to={"/register"}
+                                        >
+                                            Don't have an account?
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </div>
-            </main>
-        </div>
+                        </form>
+                    </div>
+                </main>
+            </div>
         </>
     );
 }
--- queue-management-app/src/Pages/Register.tsx
+++ queue-management-app/src/Pages/Register.tsx
@@ -1,16 +1,15 @@
 import {FC, SyntheticEvent, useState} from "react";
 import {Link} from "react-router-dom";
 import {User} from "../Models";
+import "./PagesStyle.css";
 
 
-interface RegisterProps
-{
+interface RegisterProps {
 
 }
 
-export const Register: FC<RegisterProps > = ({}) => {
+export const Register: FC<RegisterProps> = ({}) => {
     const [user, setUser] = useState<User>({
-        createdTime: new Date(),
         email: "",
         firstName: "",
         id: 0,
@@ -31,110 +30,107 @@ export const Register: FC<RegisterProps > = ({}) => {
     return (
         <>
 
-                <div className={"vertical-center"}>
-                    <main className="form-register">
-                        <img
-                            className="mb-3 align-items-center"
-                            // src={LoginLogo}
-                            alt=""
-                            width="300"
-                            height="170"
-                        />
-                        <div className="box col-12 mb-3">
-                            <form onSubmit={onSubmit}>
-                                <h1 className="page-title h3 mb-3 fw-normal">Register</h1>
+            <div className={"vertical-center"}>
+                <main className="form-box">
+                    <div className="box col-12 mb-3">
+                        <form onSubmit={onSubmit}>
+                            <h1 className="page-title h3 mb-3 fw-normal">Register</h1>
 
+                            <div className="row">
+                                <div className="col-6">
+                                    <div className="form-floating mb-3">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="firstName"
+                                            placeholder="First Name"
+                                            onChange={(e) => {
+                                                setUser({...user, firstName: e.target.value});
+                                            }}
+                                        />
+                                        <label htmlFor="floatingInput">First Name</label>
+                                    </div>
+                                </div>
+
+                                <div className="col-6">
+                                    <div className="form-floating mb-3">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="lastName"
+                                            placeholder="Last Name"
+                                            onChange={(e) => {
+                                                setUser({...user, lastName: e.target.value});
+                                            }}
+                                        />
+                                        <label htmlFor="floatingInput">Last Name</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="email"
+                                    placeholder="name@example.com"
+                                    onChange={(e) => {
+                                        setUser({...user, email: e.target.value});
+                                    }}
+                                />
+                                <label htmlFor="floatingInput">Email address</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="floatingPassword"
+                                    placeholder="Password"
+                                    onChange={(e) => {
+                                        setPassword(e.target.value);
+                                    }}
+                                />
+
+                                <label htmlFor="floatingPassword">Password</label>
+
+                            </div>
+
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="floatingConfirmPassword"
+                                    placeholder="Confirm password"
+                                    onChange={(e) => {
+                                        setConfirmPassword(e.target.value);
+                                    }}
+                                />
+                                <label htmlFor="floatingPassword">Confirm password</label>
+                            </div>
+
+                            <button
+                                className="submit-button w-100 btn btn-lg mt-3"
+                                type="submit"
+                                disabled={isSubmitted}
+                            >
+                                Register
+                            </button>
+
+                            <div className="checkbox mt-3">
                                 <div className="row">
-                                    <div className="col-6">
-                                        <div className="form-floating mb-3">
-                                            <input
-                                                type="text"
-                                                className="form-control"
-                                                id="firstName"
-                                                placeholder="First Name"
-                                                onChange={(e) => {
-                                                    setUser({...user, firstName: e.target.value});
-                                                }}
-                                            />
-                                            <label htmlFor="floatingInput">First Name</label>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-6">
-                                        <div className="form-floating mb-3">
-                                            <input
-                                                type="text"
-                                                className="form-control"
-                                                id="lastName"
-                                                placeholder="Last Name"
-                                                onChange={(e) => {
-                                                    setUser({...user, lastName: e.target.value});
-                                                }}
-                                            />
-                                            <label htmlFor="floatingInput">Last Name</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="form-floating mb-3">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="email"
-                                        placeholder="name@example.com"
-                                        onChange={(e) => {
-                                            setUser({...user, email: e.target.value});
-                                        }}
-                                    />
-                                    <label htmlFor="floatingInput">Email address</label>
-                                </div>
-                                <div className="form-floating mb-3">
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="floatingPassword"
-                                        placeholder="Password"
-                                        onChange={(e) => {
-                                            setPassword(e.target.value);
-                                        }}
-                                    />
-                                    <label htmlFor="floatingPassword">Password</label>
-                                </div>
-
-                                <div className="form-floating mb-3">
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="floatingConfirmPassword"
-                                        placeholder="Confirm password"
-                                        onChange={(e) => {
-                                            setConfirmPassword(e.target.value);
-                                        }}
-                                    />
-                                    <label htmlFor="floatingPassword">Confirm password</label>
-                                </div>
-
-                                <button
-                                    className="button-register w-100 btn btn-lg mt-3"
-                                    type="submit"
-                                    disabled={isSubmitted}
-                                >
-                                    Register
-                                </button>
-
-                                <div className="checkbox mt-3">
-                                    <div className="row">
-                                        <Link to={"/login"}
-                                            className="col-3"
-                                            style={{color: "blue"}}
+                                    <div className={"col-12"}>
+                                        <Link
+                                            className="text-link"
+                                            to={"/login"}
                                         >
                                             Already have an account? Sign In.
                                         </Link>
-                                        </div>
                                     </div>
-                            </form>
-                        </div>
-                    </main>
-                </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </main>
+            </div>
 
         </>
     );
